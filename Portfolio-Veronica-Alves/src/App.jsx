6146db0b6df45e08df1181d13c4e55070b6cd437
Veronica-Alfr <veagalves@gmail.com>
2023-05-05 16:34:32 -0300

    feat: criando page /projects

--- Portfolio-Veronica-Alves/src/App.jsx
+++ Portfolio-Veronica-Alves/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import AppStyled from "./App.styled";
+import Projects from "./pages/Projects/Projects";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
                 </Routes>
             </BrowserRouter>
         </AppStyled>
---
+++ Portfolio-Veronica-Alves/src/pages/Projects/Data/DataProjects.jsx
@@ -0,0 +1,13 @@
+import studioGhibli from "../assets/images/Project-Studio-Ghibli.jpeg";
+
+export const dataProjects = [
+    {
+        // id ?
+        url: "https://github.com/Veronica-Alfr/Studio-Ghibli",
+        title: "Studio Ghibli",
+        img: studioGhibli,
+        description: `Este projeto consome a API do Studio Ghibli para encontrar filmes 
+        através de filtros por título, localização e personagens.`,
+        technologies: ["React", "SCSS", ", React-Icons", "Eslint"],
+    },
+];
